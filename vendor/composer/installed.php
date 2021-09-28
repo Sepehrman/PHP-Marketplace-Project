eadb34786cfc7a5da2587cba464eb0b243726c38
Sepehr <sepehrman@icloud.com>
2021-09-27 19:58:51 -0700

    Final Product

---
+++ vendor/composer/installed.php
@@ -0,0 +1,47 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '1c0ea0ba134361ac4099bc051433d8a477d1d7f4',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '1c0ea0ba134361ac4099bc051433d8a477d1d7f4',
+            'dev_requirement' => false,
+        ),
+        'socketlabs/email-delivery' => array(
+            'pretty_version' => '1.2.1',
+            'version' => '1.2.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../socketlabs/email-delivery',
+            'aliases' => array(),
+            'reference' => '415580a637d059b9d56ed7ddb76d0997e4dc0ed1',
+            'dev_requirement' => false,
+        ),
+        'twbs/bootstrap' => array(
+            'pretty_version' => 'v4.3.1',
+            'version' => '4.3.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twbs/bootstrap',
+            'aliases' => array(),
+            'reference' => '8fa0d3010112dca5dd6dd501173415856001ba8b',
+            'dev_requirement' => false,
+        ),
+        'twitter/bootstrap' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => 'v4.3.1',
+            ),
+        ),
+    ),
+);
